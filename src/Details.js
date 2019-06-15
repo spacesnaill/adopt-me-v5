39ee580fe54e86765a60977ed2ba04fa32bad6b0
Patrick Geiger <patrickjgeiger@gmail.com>
2019-06-15 18:45:02 -0400

    filled out the details of the Details Class Component
    still needs to have the render portion filled out

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,29 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
 
-const Details = () => {
-  return <h1>Hi lol</h1>;
-};
+    this.state = {
+      loading: true
+    };
+  }
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false
+      });
+    }, console.error);
+  }
+  render() {
+    return;
+  }
+}
 
 export default Details;
