eec38c81aad57f9929579bb74567ef5e206fd00d
Patrick Geiger <patrickjgeiger@gmail.com>
2019-06-15 16:35:31 -0400

    cleaned up commented out code

--- src/App.js
+++ src/App.js
@@ -3,15 +3,6 @@ import { render } from "react-dom";
 import SearchParams from "./SearchParams";
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     name: "bob",
-  //     animal: "turtle",
-  //     breed: "snapping"
-  //   })
-  // ]);
-
   return (
     <div>
       <h1 id="something-imortant">Adopt Me!</h1>
