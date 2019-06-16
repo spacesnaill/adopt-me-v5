9533fdacec2b5ab3031dd4115842a3aa5a7aa173
Patrick Geiger <patrickjgeiger@gmail.com>
2019-06-16 14:03:39 -0400

    added a context to start working on a global theme

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const themeHook = useState("darkblue");
   return (
-    <div>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      <Router>
-        <SearchParams path="/"></SearchParams>
-        <Details path="/details/:id"></Details>
-      </Router>
-    </div>
+    <React.StrictMode>
+      <ThemeContext.Provider value="themeHook">
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Router>
+            <SearchParams path="/"></SearchParams>
+            <Details path="/details/:id"></Details>
+          </Router>
+        </div>
+      </ThemeContext.Provider>
+    </React.StrictMode>
   );
 };
 
