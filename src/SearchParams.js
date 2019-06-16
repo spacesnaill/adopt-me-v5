25c1f470f05a7f910a9c9fb65e9702b4df994a42
Patrick Geiger <patrickjgeiger@gmail.com>
2019-06-16 14:33:26 -0400

    implemeneted color selector in SearchParams component that uses Context to change button color throughout app

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -10,7 +10,7 @@ const SearchParams = () => {
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
   const [pets, setPets] = useState([]);
-  const [theme] = useContext(ThemeContext);
+  const [theme, setTheme] = useContext(ThemeContext);
 
   async function requestPets() {
     const { animals } = await pet.animals({
@@ -53,6 +53,20 @@ const SearchParams = () => {
         </label>
         <AnimalDropdown></AnimalDropdown>
         <BreedDropdown></BreedDropdown>
+        <label htmlFor="theme">
+          Theme
+          <select
+            value={theme}
+            onChange={e => setTheme(e.target.value)}
+            onBlur={e => setTheme(e.target.value)}
+          >
+            <option value="peru">Peru</option>
+            <option value="SteelBlue">Steel Blue</option>
+            <option value="DarkBlue">Dark Blue</option>
+            <option value="Magenta">Magenta</option>
+            <option value="Green">Green</option>
+          </select>
+        </label>
         <button style={{ backgroundColor: theme }}>Submit</button>
       </form>
       <Results pets={pets} />
