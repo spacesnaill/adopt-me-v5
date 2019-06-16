4a0117c03619152b5f48a389fe8e953581088230
Patrick Geiger <patrickjgeiger@gmail.com>
2019-06-16 13:46:01 -0400

    added the time redirect functionality to ErrorBoundary component

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,10 +1,10 @@
 // mostly code from https://reactjs.org/docs/error-boundaries.html
 
 import React, { Component } from "react";
-import { Link } from "@reach/router";
+import { Link, Redirect } from "@reach/router";
 
 class ErrorBoundary extends Component {
-  state = { hasError: false };
+  state = { hasError: false, redirect: false };
   static getDerivedStateFromError() {
     return { hasError: true };
   }
@@ -13,7 +13,18 @@ class ErrorBoundary extends Component {
     console.error("ErrorBoundary Caught an Error", error, info);
   }
 
+  componentDidUpdate() {
+    if (this.state.hasError) {
+      setTimeout(() => {
+        this.setState({ redirect: true });
+      }, 5000);
+    }
+  }
+
   render() {
+    if (this.state.redirect) {
+      return <Redirect to="/"></Redirect>;
+    }
     if (this.state.hasError) {
       return (
         <h1>
